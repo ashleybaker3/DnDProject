7bd216dd30b310084f33164b3dc5d4c2547380b5
hope-crichlow <hopecrichlow@gmail.com>
2021-07-08 18:02:00 -0600

    throwing an error on return

--- races.py
+++ races.py
@@ -1,54 +1,58 @@
-# import DnD
-
-class Character:
-
-    def __init__(self, name: str, strength: int, dexterity: int, consitution: int, wisdom: int, charisma: int, hp: int = 0, xp: int = 0, level: int = 1):
-        self.name = name
-        self.strength = strength
-        self.dexterity = dexterity
-        self.constitution = consitution
-        self.wisdom = wisdom
-        self.charisma = charisma
-        self.hp = hp
-        self.xp = xp
-        self.level = level
 
 
-    def attack(self, key: str, target: object):
-        pass
+# class Character:
 
-    def damage(self, damage):
-        self.hp -= damage
+#     def __init__(self, name: str, strength: int, dexterity: int, consitution: int, wisdom: int, charisma: int, hp: int = 0, xp: int = 0, level: int = 1):
+#         self.name = name
+#         self.strength = strength
+#         self.dexterity = dexterity
+#         self.constitution = consitution
+#         self.wisdom = wisdom
+#         self.charisma = charisma
+#         self.hp = hp
+#         self.xp = xp
+#         self.level = level
 
-    def levelUp(self):
-        self.level += 1
-        print(f"Yay! {self.name} leveled up!")
 
-    def savingThrow(self):
-        pass
+#     def attack(self, key: str, target: object):
+#         pass
 
-    def death(self):
-        print(f"{self.name} died. :(")
+#     def damage(self, damage):
+#         self.hp -= damage
 
+#     def levelUp(self):
+#         self.level += 1
+#         print(f"Yay! {self.name} leveled up!")
+
+#     def savingThrow(self):
+#         pass
+
+#     def death(self):
+#         print(f"{self.name} died. :(")
+from DnD import Character
+from fightingClass import *
 
 # Race subclass
 # Race [Dwarf]: modifies base stats (except xp and level) ref. players handbook.
 
 class Dwarf(Character):
 
-    def __init__(self, name: str, strength: int, dexterity: int, consitution: int, wisdom: int, charisma: int, hp: int = 0, xp: int = 0, level: int = 1):
+    def __init__(self, name: str, fighting_class: dict, strength: int = 10, dexterity: int = 10, consitution: int = 10, wisdom: int = 10, charisma: int = 10, stamina: int = 10, mana: int = 10, hp: int = 0, xp: int = 0, level: int = 1):
 
-        super().__init__(name, strength, dexterity, consitution, wisdom, charisma, hp )
+        super().__init__(name, strength, fighting_class, dexterity, consitution, wisdom, charisma, hp)
 
         self.strength = strength + 1
+        self.fighting_class = fighting_class
         self.dexterity = dexterity
         self.constitution = consitution + 2
         self.wisdom = wisdom + 1
         self.charisma = charisma
-        self.hp = 10
+        self.hp = hp
 
         print(self)
         return self
 
-        
-herring = Dwarf("Herring", 10, 10, 10, 6, 4, 7)
+
+
+herring = Dwarf(name="Herring", fighting_class= wizard)
+print(herring)
