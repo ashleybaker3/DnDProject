f4e36a39be1dc25378dd057d0c692d1ee12ee641
hope-crichlow <hopecrichlow@gmail.com>
2021-07-08 18:27:03 -0600

    TypeError: __init__() should return None...

--- races.py
+++ races.py
@@ -1,3 +1,13 @@
+from random import randrange
+from fightingClass import fightingClass
+
+
+
+wizard = {
+    "cantrip": fightingClass("Ray of Frost", randrange(1,7), 0, 1),
+    "spell": fightingClass("Magic Missile", randrange(1,7), 0, 1)
+}
+
 class Character:
 
     def __init__(self, name: str, strength: int, dexterity: int, consitution: int, wisdom: int, charisma: int, hp: int = 0, xp: int = 0, level: int = 1):
@@ -14,8 +24,7 @@ class Character:
 class Dwarf(Character):
 
     def __init__(self, name: str, strength: int, dexterity: int, consitution: int, wisdom: int, charisma: int, hp: int = 0, xp: int = 0, level: int = 1):
-
-        super().__init__(name, strength, dexterity, consitution, wisdom, charisma, hp )
+        super().__init__(self, name, strength, dexterity, consitution, wisdom, charisma, hp)
 
         self.strength = strength + 1
         self.dexterity = dexterity
@@ -26,3 +35,7 @@ class Dwarf(Character):
 
         print(self)
         return self
+            
+
+Gideon = Dwarf("Gideon", 5, 7, 3,9,7)
+print(Gideon)
